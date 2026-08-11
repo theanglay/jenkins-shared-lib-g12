c831f733a88a0692ff74d478aef1e385906f3a0c
Theanglay <he.theanglay@gmail.com>
2026-08-11 15:38:37 +0000

    fix

--- vars/scanSonarqube.groovy
+++ vars/scanSonarqube.groovy
@@ -1,17 +1,15 @@
-// for scaning code with sonarqube 
-// For Scaning Reactjs, Nextjs project with sonarqube 
-def call(String projectName,String projectVersion, String projectKey){
-withSonarQubeEnv(credentialsId: 'SONARQUBE-TOKEN', installationName: 'sonar-scanner') {
-    script{
-    
-        sh """
-
-        ${scannerHome}/bin/sonar-scanner \
-            -Dsonar.projectName="${projectName}" \
-            -Dsonar.projectKey=${projectKey} \
-            -Dsonar.projectVersion=${projectVersion}
-
-        """
-    }     
-}
+def call(String projectName, String projectVersion, String projectKey) {
+    withSonarQubeEnv(credentialsId: 'SONARQUBE-TOKEN', installationName: 'sonar-scanner') {
+        script {
+            // Retrieve the scanner home directory dynamically
+            def scannerHome = tool 'sonar-scanner' 
+            
+            sh """
+            ${scannerHome}/bin/sonar-scanner \
+                -Dsonar.projectName="${projectName}" \
+                -Dsonar.projectKey="${projectKey}" \
+                -Dsonar.projectVersion="${projectVersion}"
+            """
+        }     
+    }
 }
